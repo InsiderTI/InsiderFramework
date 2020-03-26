5e4cc40f0d3d6d4a8d0a4a620b3119500b80cc34
Marcello Costa <marcello88costa@yahoo.com.br>
2020-03-26 17:21:28 -0300

    Removing non-used files

--- packs/start/controllers/main_controller.php
+++ packs/start/controllers/main_controller.php
@@ -21,21 +21,9 @@ class Main_Controller extends \KeyClass\Controller {
        @package Controllers\start\Main_Controller
 
        @Route(path="home")
-       @Permission(type="custom", rules="homeRule")
        @Cache(none)
     */
     public function home() {
-        // Requesting test model
-        $ExampleModel=\KeyClass\Request::Model($this->pack.'::Example', BD_APP);
-
-        $ReturnOfModel=$ExampleModel->QueryTest();
-
-        // Send info to viewBag
-        $ReturnOfModel = array(
-            0 => 'Test'
-        );
-        $this->addViewBag($ReturnOfModel,'ReturnOfModel');
-
         // Renderview
         $this->renderView('start::home.sgv');
     }
