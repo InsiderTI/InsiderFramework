e5d59fcc1e64b755eb8109187dafc7b8a2c09b38
Marcello TI <marcello88costa@yahoo.com.br>
2020-07-01 20:48:04 -0300

    Create original images for assets in sys
    #48

--- Apps/Sys/Views/error/friendlyStandardErrorMsg.php
+++ Apps/Sys/Views/error/friendlyStandardErrorMsg.php
@@ -16,7 +16,7 @@
         <div id="all" style='text-align: center;' unselectable='on' onselectstart='return false;' onmousedown='return false;'>
             <h2>This page is experiencing some problems.</h2><br/>
             <h3>Come back later please.</h3>
-            <img style='width: 15em;' src='<?php echo REQUESTED_URL . "/Apps/Sys/assets/img/error_frame_cartoon.png"; ?>' />
+            <img style='width: 15em;' src='<?php echo REQUESTED_URL . "/Apps/Sys/assets/img/500_user_error.png"; ?>' />
         </div>
     </body>
 </html>
