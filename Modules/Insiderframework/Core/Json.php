eb8706f900e6795757c2ed8477d19b671f578b41
Marcello Costa <marcello88costa@yaho.com.br>
2021-02-16 00:00:06 -0300

    Json read logic

---
+++ Modules/Insiderframework/Core/Json.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Insiderframework\Core;
+
+/**
+ * Methods responsible for handle files and directories
+ *
+ * @author Marcello Costa
+ *
+ * @package Modules\Insiderframework\Core\Json
+ *
+ */
+class Json
+{
+    use \Modules\Insiderframework\Core\Json\JsonTrait;
+}
---
+++ Modules/Insiderframework/Core/Json/JsonTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Modules\Insiderframework\Core\Json;
+
+/**
+ * Methods responsible for handle JSON
+ *
+ * @author  Marcello Costa
+ *
+ * @package Modules\Insiderframework\Core\Json\JsonTrait
+ */
+trait JsonTrait
+{
+    /**
+     * Get the data of a JSON file
+     *
+     * @author Marcello Costa
+     *
+     * @package Modules\Insiderframework\Core\Json\JsonTrait
+     *
+     * @param string $filepath Path of the JSON file
+     * @param bool   $assoc    If this is true the function will return
+     *                         an associative array instead of an object
+     *
+     * @return array|bool Data of JSON file if the file can be read.
+     *                    If not, returns false.
+     */
+    public static function getJSONDataFile(string $filepath, bool $assoc = true)
+    {
+        if (file_exists($filepath)) {
+            $filecontent = \Modules\Insiderframework\Core\FileTree::fileReadContent($filepath);
+
+            $t = json_decode($filecontent);
+
+            if ($t === null) {
+                return false;
+            }
+
+            return (json_decode($filecontent, $assoc));
+        } else {
+            return false;
+        }
+    }
+}
