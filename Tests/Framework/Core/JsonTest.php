b9e06d87d1cb808cc751f46c5551e9e9ca55e013
Marcello Costa <marcello88costa@yaho.com.br>
2021-02-16 00:00:06 -0300

    Json read logic

---
+++ Tests/Framework/Core/JsonTest.php
@@ -0,0 +1,27 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use \Modules\Insiderframework\Core\Json;
+
+final class JsonTest extends TestCase
+{
+  public function testShouldReadJsonAndReturnArray(): void
+  {
+    $jsonContent = Json::getJSONDataFile(
+        "Config" . DIRECTORY_SEPARATOR .
+        "core.json"
+    );
+
+    $this->assertEquals('array', gettype($jsonContent));
+  }
+
+  public function testShouldReadJsonAndReturnObject(): void
+  {
+    $jsonContent = Json::getJSONDataFile(
+        "Config" . DIRECTORY_SEPARATOR .
+        "core.json",
+        false
+    );
+
+    $this->assertEquals('object', gettype($jsonContent));
+  }
+}
